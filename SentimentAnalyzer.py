ac2c3e41d504f8c46dae7a4b0399f6d85758b06e
mohammad <mohammadhosseini4621@gmail.com>
2023-09-14 13:52:39 -0700

    2 - Download the pre-trained BERT model for emotion recognition

--- SentimentAnalyzer.py
+++ SentimentAnalyzer.py
@@ -1,3 +1,8 @@
 import torch
 from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import pipeline
+
+# دانلود مدل پیش‌آموزش‌داده‌شده BERT برای تشخیص احساسات
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForSequenceClassification.from_pretrained(model_name)
