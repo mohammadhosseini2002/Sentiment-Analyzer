f2adbdb82de0cd236e5621645855501f99ef0c7e
mohammad <mohammadhosseini4621@gmail.com>
2023-09-14 13:53:46 -0700

    3 - A function to detect emotions from text

--- SentimentAnalyzer.py
+++ SentimentAnalyzer.py
@@ -6,3 +6,25 @@ from transformers import pipeline
 model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
 tokenizer = BertTokenizer.from_pretrained(model_name)
 model = BertForSequenceClassification.from_pretrained(model_name)
+
+# تابعی برای تشخیص احساسات از متن
+def detect_sentiment(text):
+    # توکن‌بندی متن
+    tokens = tokenizer.encode(text, add_special_tokens=True, truncation=True, padding=True, return_tensors="pt")
+
+    # اجرای مدل بر روی متن
+    with torch.no_grad():
+        outputs = model(tokens)
+
+    # استخراج احساس از نتایج مدل
+    prediction = torch.argmax(outputs.logits, dim=1).item()
+    
+    # ترجمه‌ی احساس به عبارت معنایی
+    if prediction == 0:
+        sentiment = "Negative"
+    elif prediction == 2:
+        sentiment = "Neutral"
+    else:
+        sentiment = "Positive"
+
+    return sentiment
